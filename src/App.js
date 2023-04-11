c53e9ac27909935b202d8042976f72bad2326a05
NehaParakh1 <nehadugar3@gmail.com>
2023-04-11 18:37:57 +0530

    1_login and signup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useContext} from 'react';
+import Login from './Components/Auth/Login';
+import Header from './Components/Header/Header';
+import AuthContext from './Components/Store/AuthContext';
 
 function App() {
+  const authCtx = useContext(AuthContext)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+    <Header/>
+    {!authCtx.isLoggedIn && <Login/>}
+    </>
   );
 }
 
---
+++ src/Components/Store/AuthContext.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+const AuthContext = React.createContext({
+  token: "",
+  isLoggedIn: false,
+  login: (token) => {},
+  logout: () => {},
+});
+
+export const AuthContextProvider = (props) => {
+  let initialToken = localStorage.getItem("token");
+  const [token, setToken] = useState(initialToken);
+
+  const userIsLoggedIn = !!token;
+
+  const loginHandler = (token) => {
+    console.log(token)
+    setToken(token);
+    localStorage.setItem("token", token);
+  };
+  const logoutHandler = () => {
+    setToken(null);
+    localStorage.removeItem("token");
+    localStorage.removeItem("date");
+    console.log(token);
+  };
+
+  const contextValue = {
+    token: token,
+    isLoggedIn: userIsLoggedIn,
+    login: loginHandler,
+    logout: logoutHandler,
+  };
+
+  return (
+    <AuthContext.Provider value={contextValue}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+export default AuthContext;
