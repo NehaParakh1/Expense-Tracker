ba9c76a8fa72448220f6d492f9cff902b5bd796d
NehaParakh1 <nehadugar3@gmail.com>
2023-04-12 11:07:34 +0530

    02_login screen

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 import { AuthContextProvider } from './Components/Store/AuthContext';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,8 +10,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <AuthContextProvider>
+    <BrowserRouter>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </BrowserRouter>
   </AuthContextProvider>
 );
